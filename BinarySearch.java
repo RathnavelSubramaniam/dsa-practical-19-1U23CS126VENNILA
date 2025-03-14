19ead9b9e6beb824c6016d8991e3ed832a75e902
1U23CS126VENNILA <142867008+1U23CS126VENNILA@users.noreply.github.com>
2025-03-14 05:39:03 +0000

    submit

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,6 +1,6 @@
 import java.util.*;
 public class BinarySearch {
-public static void main(String[] args) 
+public static void main(String[] args)
 {
 System.out.print("BINARY SEARCH\n");
 Scanner in = new Scanner(System.in);
@@ -10,14 +10,13 @@ int[] numbers=new int[n];
 System.out.println("ENTER THE INTEGER VALUES:");
 for(int i=0;i<n;i++)
 {
- if(in.hasNextInt()) 
- numbers[i]=in.nextInt();
- else
- {
- System.out.println("Enter integer numbers only");
- System.exit(0);
-   }
-        }
+if(in.hasNextInt())
+numbers[i]=in.nextInt();
+else
+{
+System.out.println("Enter integer numbers only");
+System.exit(0);
+ } }
 System.out.println("ENTER THE KEY VALUE TO BE SEARCHED IN BINARY SEARCH LIST");
 int key = in.nextInt();
 int res=BinarySearchIterative(numbers,key);
@@ -25,9 +24,14 @@ if(res==-1)
 System.out.println(key + " IS NOT FOUND");
 else
 System.out.println(key + " FOUND IN THE ARRAY AT POSITION "+(res+1));
-in.close();
-}
-public static int BinarySearchIterative( ) {
-// TYPE YOUR CODE HERE
-}
-}
+in.close();}
+public static int BinarySearchIterative(int[] A, int data) {
+int low = 0, high = A.length-1,mid;
+while (low <= high) {
+mid = low + (high-low)/2; //To avoid overflow
+if(A[mid] == data)
+return mid;
+else if(A[mid] < data)
+low = mid + 1;
+else high = mid - 1;}
+return -1;}}
